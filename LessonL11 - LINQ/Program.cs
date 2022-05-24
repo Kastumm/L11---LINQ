117d697f60a39285ef28840935514eff059d7a2c
Kastumm <emilpirlea@mail.com>
2022-05-24 11:28:42 +0300

    Add project files.

---
+++ LessonL11 - LINQ/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace LessonL11
+{
+    public static class AssignmentL11
+    {
+        //Closures
+        public static void Main(string[] args)
+        {
+            var inc = GetAFunc();
+            Console.WriteLine(inc(5));
+            Console.WriteLine(inc(6));
+
+        }
+
+        public static Func<int, int> GetAFunc()
+        {
+            var myVar = 1;
+            Func<int, int> inc = delegate (int var1)
+            {
+                myVar = myVar + 1;
+                return var1 + myVar;
+            };
+            return inc;
+        }
+    }
+
+    //LINQ Implemented in my project
+
+
+}
